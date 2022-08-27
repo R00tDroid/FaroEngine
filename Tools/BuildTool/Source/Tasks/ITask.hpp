9552609788ee8d00b43b134791e6041738f6a3e3
SanderBrouwers <sanderbrouwers1@live.nl>
2022-08-27 18:40:54 +0200

    Document task interface

--- Tools/BuildTool/Source/Tasks/ITask.hpp
+++ Tools/BuildTool/Source/Tasks/ITask.hpp
@@ -1,9 +1,15 @@
 #pragma once
+#include "ProjectManifest.hpp"
 
+// Interface for a task. Tasks define a specific action that will be performed (generate solution, build project, clean project, etc).
 class ITask
 {
 public:
+    virtual ~ITask() = default;
+
+    // Get the task pririty. This is used to determine the execution order (clean before build for exmaple).
     virtual int GetPriority() = 0;
 
+    // Perform the task action/
     virtual bool Run(ProjectManifest& project) = 0;
 };
