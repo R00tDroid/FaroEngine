998c0c906850c242e43577023c91504121aa25f8
SanderBrouwers <sanderbrouwers1@live.nl>
2022-08-28 14:54:53 +0200

    Parse project name

--- Tools/BuildTool/Source/ProjectManifest.hpp
+++ Tools/BuildTool/Source/ProjectManifest.hpp
@@ -26,14 +26,46 @@ public:
             return false;
         }
 
-        picojson::value rootObject;
-        std::string error = picojson::parse(rootObject, fileStream);
+        picojson::value rootValue;
+        std::string error = picojson::parse(rootValue, fileStream);
         if (!error.empty())
         {
             Utility::Print("JSON parsing error: " + error);
             return false;
         }
 
+        if (!rootValue.is<picojson::object>())
+        {
+            Utility::PrintLine("Expected a JSON object as root of the project manifest");
+            return false;
+        }
+
+        picojson::object& rootObject = rootValue.get<picojson::object>();
+
+        if (!ParseProject(rootObject)) return false;
+        if (!ParseModules(rootObject)) return false;
+
+        return true;
+    }
+
+    bool ParseProject(picojson::object& rootObject)
+    {
+        if (rootObject.find("Name") != rootObject.end())
+        {
+            picojson::value& value = rootObject["Name"];
+            if (!value.is<std::string>())
+            {
+                Utility::PrintLine("Expected Name to be a string");
+                return false;
+            }
+            projectName = value.get<std::string>();
+        }
+
+        return true;
+    }
+
+    bool ParseModules(picojson::object& rootObject)
+    {
         return true;
     }
 };
