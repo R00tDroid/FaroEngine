fa9fa56a180251102a007d4d708929a05ac0bb9b
R00tDroid <info@rootdroid.nl>
2022-09-03 11:21:34 +0200

    Fix typo

--- Tools/BuildTool/Source/Tasks/ITask.hpp
+++ Tools/BuildTool/Source/Tasks/ITask.hpp
@@ -7,7 +7,7 @@ class ITask
 public:
     virtual ~ITask() = default;
 
-    // Get the task pririty. This is used to determine the execution order (clean before build for exmaple).
+    // Get the task priority. This is used to determine the execution order (clean before build for exmaple).
     virtual int GetPriority() = 0;
 
     // Perform the task action/
