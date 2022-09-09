da44c41a3be8abe9e1607a53999f3af7ecd56f75
R00tDroid <info@rootdroid.nl>
2022-09-09 21:05:50 +0200

    Setup application class and lifecycle

--- Source/Engine/Platform/PlatformBase/Source/Application.h
+++ Source/Engine/Platform/PlatformBase/Source/Application.h
@@ -1,1 +1,20 @@
 #pragma once
+
+namespace Faro
+{
+    class Application
+    {
+    public:
+        Application();
+        ~Application();
+
+        int Run();
+
+    private:
+        bool Init();
+        bool Update();
+        bool Destroy();
+    };
+
+    extern Application* GApplication;
+}
