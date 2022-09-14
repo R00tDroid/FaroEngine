ee8468f858271877d44c25e8412d8f1fdfc63b21
R00tDroid <info@rootdroid.nl>
2022-09-14 18:40:51 +0200

    Format log message

--- Source/Engine/Core/Source/Log.cpp
+++ Source/Engine/Core/Source/Log.cpp
@@ -4,6 +4,9 @@ namespace Faro
 {
     void Log(LogTag tag, ELogCategory category, String format, ...)
     {
-
+        va_list args;
+        va_start(args, format);
+        String message = FormatStringVA(format, args);
+        va_end(args);
     }
 }
