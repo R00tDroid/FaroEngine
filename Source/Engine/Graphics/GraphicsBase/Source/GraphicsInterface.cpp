61618725994b2ac7b4d765b2c19ee1069feb1124
R00tDroid <info@rootdroid.nl>
2022-09-19 21:07:44 +0200

    Implement graphics interface reflection

--- Source/Engine/Graphics/GraphicsBase/Source/GraphicsInterface.cpp
+++ Source/Engine/Graphics/GraphicsBase/Source/GraphicsInterface.cpp
@@ -2,4 +2,12 @@
 
 namespace Faro
 {
+    IMPLEMENT_INSTANCE_REGISTRY(GraphicsInterface, Faro::GraphicsInterface)
+
+    GraphicsInterface* GGraphics = nullptr;
+
+    Array<GraphicsInterface*> GetGraphicsInterfaces()
+    {
+        return InstanceRegistry_GraphicsInterface::Get().GetValues();
+    }
 }
--- Source/Engine/Graphics/GraphicsBase/Source/GraphicsInterface.hpp
+++ Source/Engine/Graphics/GraphicsBase/Source/GraphicsInterface.hpp
@@ -1,5 +1,18 @@
 #pragma once
+#include <Containers/Array.hpp>
+#include <Util/ClassRegistry.hpp>
 
 namespace Faro
 {
+    class GraphicsInterface
+    {
+        
+    };
+
+    extern GraphicsInterface* GGraphics;
+
+    extern Array<GraphicsInterface*> GetGraphicsInterfaces();
+
+    DEFINE_INSTANCE_REGISTRY(GraphicsInterface, Faro::GraphicsInterface)
+#define REGISTER_GRAPHICS(TYPE) REGISTER_INSTANCE(Platform, TYPE)
 }
