eecd1e77f5ecf57bf79352246647956c9f0b67b4
R00tDroid <info@rootdroid.nl>
2022-09-20 17:12:01 +0200

    Enable logging

--- Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsAdapterD3D12.cpp
+++ Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsAdapterD3D12.cpp
@@ -8,6 +8,16 @@ namespace Faro
 
         D3D12CreateDevice(dxgiAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
 
+        ID3D12InfoQueue* infoQueue = nullptr;
+        if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue))))
+        {
+            infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, TRUE);
+            infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE);
+            infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE);
+
+            infoQueue->Release();
+        }
+
         D3D12_COMMAND_QUEUE_DESC queueDesc = {};
         queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
         queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
