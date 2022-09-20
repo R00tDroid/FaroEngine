6bdb350281320da54e2f8b2d2e495f705607bccb
R00tDroid <info@rootdroid.nl>
2022-09-20 17:34:47 +0200

    assign adapter

--- Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsCommandListD3D12.cpp
+++ Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsCommandListD3D12.cpp
@@ -2,6 +2,10 @@
 
 namespace Faro
 {
+    GraphicsCommandListD3D12::GraphicsCommandListD3D12(GraphicsAdapterD3D12* inAdapter) : adapter(inAdapter)
+    {
+    }
+
     void GraphicsCommandListD3D12::Reset()
     {
     }
--- Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsCommandListD3D12.hpp
+++ Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsCommandListD3D12.hpp
@@ -1,12 +1,19 @@
 #pragma once
 #include <GraphicsCommandList.hpp>
 
+#include "GraphicsAdapterD3D12.hpp"
+
 namespace Faro
 {
     class GraphicsCommandListD3D12 : public GraphicsCommandList
     {
     public:
+        GraphicsCommandListD3D12(GraphicsAdapterD3D12*);
+
         void Reset() override;
         void Execute() override;
+
+    private:
+        GraphicsAdapterD3D12* adapter = nullptr;
     };
 }
