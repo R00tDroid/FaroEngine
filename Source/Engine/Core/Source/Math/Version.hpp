0a42433813d3a77149afabaa0e401839bd57ea54
R00tDroid <info@rootdroid.nl>
2022-09-21 22:05:43 +0200

    Fix invalid include path

--- Source/Engine/Core/Source/Math/Version.hpp
+++ Source/Engine/Core/Source/Math/Version.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include <Primitives.hpp>
-#include "String.hpp"
+#include <Containers/String.hpp>
 
 namespace Faro
 {
     struct Version
     {
-        Version(uint16 major, uint16 minor, uint16 patch, String commit, String branch = "") : major(major), minor(minor), patch(patch), commit(commit), branch(branch) {}
+        Version(uint16 major, uint16 minor, uint16 revision, String commit, String branch = "") : major(major), minor(minor), patch(revision), commit(commit), branch(branch) {}
 
         uint16 major;
         uint16 minor;
