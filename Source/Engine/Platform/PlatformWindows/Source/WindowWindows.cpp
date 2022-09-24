8f5717800f14ac079387483ce0c583333e9a8a9d
R00tDroid <info@rootdroid.nl>
2022-09-24 22:00:48 +0200

    Setup window thread

--- Source/Engine/Platform/PlatformWindows/Source/WindowWindows.cpp
+++ Source/Engine/Platform/PlatformWindows/Source/WindowWindows.cpp
@@ -2,13 +2,27 @@
 
 namespace Faro
 {
+    void WindowThread::ThreadInit()
+    {
+    }
+
+    void WindowThread::ThreadUpdate()
+    {
+    }
+
+    void WindowThread::ThreadDestroy()
+    {
+    }
+
     void WindowWindows::Init()
     {
         Window::Init();
+        windowThread.Start();
     }
 
     void WindowWindows::Destroy()
     {
+        windowThread.WaitForStop();
         Window::Destroy();
     }
 
--- Source/Engine/Platform/PlatformWindows/Source/WindowWindows.hpp
+++ Source/Engine/Platform/PlatformWindows/Source/WindowWindows.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include <Window.hpp>
+#include <Thread.hpp>
 
 namespace Faro
 {
+    class WindowThread : public IThread
+    {
+    protected:
+        void ThreadInit() override;
+        void ThreadUpdate() override;
+        void ThreadDestroy() override;
+    };
+
     class WindowWindows : public Window
     {
     public:
@@ -12,5 +21,16 @@ namespace Faro
         void SetTitle(String title) override;
         void SetSize(Int2D size) override;
         Int2D GetSize() override;
+
+    private:
+        struct WindowState
+        {
+            Int2D position = Int2D::zero;
+            Int2D size = Int2D::zero;
+        };
+
+        WindowState windowStates[WindowMode::WM_ENUMSIZE];
+
+        WindowThread windowThread;
     };
 }
