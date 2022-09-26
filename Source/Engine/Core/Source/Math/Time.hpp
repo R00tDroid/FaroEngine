e35e75f92d1a410f11c804bea4538976d3cb8c30
R00tDroid <info@rootdroid.nl>
2022-09-26 17:27:56 +0200

    Expand time interface

--- Source/Engine/Core/Source/Math/Time.hpp
+++ Source/Engine/Core/Source/Math/Time.hpp
@@ -7,17 +7,26 @@ namespace Faro
     class Duration
     {
     public:
+        static Duration FromHours(int16 hours) { return Duration(std::chrono::system_clock::duration(std::chrono::hours(hours))); }
+        static Duration FromMinutes(int16 hours) { return Duration(std::chrono::system_clock::duration(std::chrono::minutes(hours))); }
+        static Duration FromSeconds(int16 hours) { return Duration(std::chrono::system_clock::duration(std::chrono::seconds(hours))); }
+        static Duration FromMilliseconds(int16 hours) { return Duration(std::chrono::system_clock::duration(std::chrono::milliseconds(hours))); }
+        static Duration FromMicroseconds(int16 hours) { return Duration(std::chrono::system_clock::duration(std::chrono::microseconds(hours))); }
+
         Duration(std::chrono::steady_clock::duration duration) : duration(duration){}
 
-        int16 TotalMinutes();
-        float TotalSeconds();
-        float TotalMilliseconds();
-        int32 TotalMicroseconds();
+        int32 TotalHours();
+        int32 TotalMinutes();
+        int64 TotalSeconds();
+        int64 TotalMilliseconds();
+        int64 TotalMicroseconds();
 
-        int16 Minutes();
-        int16 Seconds();
+        int8 Hours();
+        int8 Minutes();
+        int8 Seconds();
         int16 Milliseconds();
-        int32 Microseconds();
+        int16 Microseconds();
+
     private:
         std::chrono::steady_clock::duration duration;
     };
