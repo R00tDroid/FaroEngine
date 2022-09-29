33f7dcc30b128d794ffbf8a4f4c6d117b6bbbf22
R00tDroid <info@rootdroid.nl>
2022-09-29 18:15:26 +0200

    Add GUI with sciter

---
+++ Tools/BuildToolGUI/Source/Main.cpp
@@ -0,0 +1,33 @@
+#include "sciter-x.h"
+#include "sciter-x-window.hpp"
+
+class frame: public sciter::window {
+public:
+  frame() : window(SW_TITLEBAR | SW_RESIZEABLE | SW_CONTROLS | SW_MAIN | SW_ENABLE_DEBUG) {}
+
+  // passport - lists native functions and properties exposed to script under 'frame' interface name:
+  SOM_PASSPORT_BEGIN(frame)
+    SOM_FUNCS(
+      SOM_FUNC(nativeMessage)
+    )
+  SOM_PASSPORT_END
+  
+  // function expsed to script:
+  sciter::string  nativeMessage() { return WSTR("Hello C++ World"); }
+
+};
+
+#include "Resources.generated.hpp"
+
+int uimain(std::function<int()> run ) {
+
+  sciter::archive::instance().open(aux::elements_of(resources));
+
+  sciter::om::hasset<frame> pwin = new frame();
+
+  pwin->load(WSTR("this://app/index.html"));
+  
+  pwin->expand();
+
+  return run();
+}
