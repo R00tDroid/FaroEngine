a79f4d1114a503bdc2597a704ffc64ac02e2b4a3
R00tDroid <info@rootdroid.nl>
2022-09-29 21:05:12 +0200

    Delete electron project

--- Tools/ToolGUI/main/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI',{
-    runBuildTool: (command) => ipcRenderer.invoke('action:runBuildTool', command),
-    onLogClear: (callback) => ipcRenderer.on('event:onLogClear', callback),
-    onLogAppend: (callback) => ipcRenderer.on('event:onLogAppend', callback)
-})
