26b9df3e5c735d2e3e37662f0203648d7ddd8146
R00tDroid <info@rootdroid.nl>
2022-09-29 18:20:44 +0200

    Remove window border

--- Tools/BuildToolGUI/Source/Main.cpp
+++ Tools/BuildToolGUI/Source/Main.cpp
@@ -1,33 +1,34 @@
 #include "sciter-x.h"
 #include "sciter-x-window.hpp"
 
-class frame: public sciter::window {
+class frame: public sciter::window
+{
 public:
-  frame() : window(SW_TITLEBAR | SW_RESIZEABLE | SW_CONTROLS | SW_MAIN | SW_ENABLE_DEBUG) {}
+    frame() : window(SW_ALPHA | SW_POPUP | SW_MAIN | SW_ENABLE_DEBUG) {}
 
-  // passport - lists native functions and properties exposed to script under 'frame' interface name:
-  SOM_PASSPORT_BEGIN(frame)
+    // passport - lists native functions and properties exposed to script under 'frame' interface name:
+    SOM_PASSPORT_BEGIN(frame)
     SOM_FUNCS(
-      SOM_FUNC(nativeMessage)
+        SOM_FUNC(nativeMessage)
     )
-  SOM_PASSPORT_END
-  
-  // function expsed to script:
-  sciter::string  nativeMessage() { return WSTR("Hello C++ World"); }
+    SOM_PASSPORT_END
+
+    // function expsed to script:
+    sciter::string  nativeMessage() { return WSTR("Hello C++ World"); }
 
 };
 
 #include "Resources.generated.hpp"
 
-int uimain(std::function<int()> run ) {
+int uimain(std::function<int()> run )
+{
+    sciter::archive::instance().open(aux::elements_of(resources));
 
-  sciter::archive::instance().open(aux::elements_of(resources));
+    sciter::om::hasset<frame> pwin = new frame();
 
-  sciter::om::hasset<frame> pwin = new frame();
+    pwin->load(WSTR("this://app/index.html"));
 
-  pwin->load(WSTR("this://app/index.html"));
-  
-  pwin->expand();
+    pwin->expand();
 
-  return run();
+    return run();
 }
