a32703c4c79f9168b0ce7b22eab1dc40ae309c75
R00tDroid <info@rootdroid.nl>
2022-10-01 16:07:08 +0200

    Parse and dispatch project path

--- Tools/BuildToolGUI/Source/Main.cpp
+++ Tools/BuildToolGUI/Source/Main.cpp
@@ -2,6 +2,7 @@
 #include "sciter-x.h"
 #include "Window.hpp"
 #include "Console.hpp"
+#include <shellapi.h>
 
 int uimain(std::function<int()> run)
 {
@@ -12,6 +13,23 @@ int uimain(std::function<int()> run)
     window->load(WSTR("this://app/index.html"));
 
     window->expand();
+
+    std::wstring commandLine = GetCommandLineW();
+
+    int argc = 0;
+    LPWSTR* argv = CommandLineToArgvW(commandLine.c_str(), &argc);
+
+    if (argc > 1)
+    {
+        std::filesystem::path projectPath = argv[1];
+        projectPath = std::filesystem::absolute(projectPath);
+        projectPath = std::filesystem::weakly_canonical(projectPath);
+
+        BEHAVIOR_EVENT_PARAMS params = { 0 };
+        params.name = WSTR("setProjectPath");
+        params.data = projectPath.string();
+        AppWindow::broadcast_event(params);
+    }
 
     Console::Init();
 
