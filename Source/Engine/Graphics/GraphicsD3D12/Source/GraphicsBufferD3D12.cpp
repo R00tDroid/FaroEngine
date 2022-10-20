481d39c050b6c2ac7589c62af5797f256f40e444
R00tDroid <info@rootdroid.nl>
2022-10-20 18:11:55 +0200

    Create and destroy buffers

---
+++ Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsBufferD3D12.cpp
@@ -0,0 +1,83 @@
+#include "GraphicsBufferD3D12.hpp"
+#include "GraphicsAdapterD3D12.hpp"
+#include <directx/d3dx12.h>
+
+#include "Memory/MemoryManager.hpp"
+
+namespace Faro
+{
+    void IGraphicsBufferD3D12::Destroy()
+    {
+        gpuResource->Release();
+        gpuResource = nullptr;
+
+        cpuAddress = nullptr;
+
+        GraphicsBuffer::Destroy();
+    }
+
+    void GraphicsBufferUploadD3D12::Init()
+    {
+        IGraphicsAdapterChild::Init();
+
+        D3D12_HEAP_PROPERTIES heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+        D3D12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(GetDesc().dataSize);
+
+        GetTypedAdapter<GraphicsAdapterD3D12>()->GetDevice()->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&gpuResource));
+        gpuResource->SetName(L"GraphicsBufferUploadD3D12");
+    }
+
+    void GraphicsBufferRemoteD3D12::Init()
+    {
+        IGraphicsAdapterChild::Init();
+
+        D3D12_HEAP_PROPERTIES heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+        D3D12_RESOURCE_DESC resourceDesc = GetNativeDesc();
+
+        GetTypedAdapter<GraphicsAdapterD3D12>()->GetDevice()->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&gpuResource));
+        gpuResource->SetName(L"GraphicsBufferRemoteD3D12");
+    }
+
+    D3D12_RESOURCE_DESC GraphicsBufferRemoteD3D12::GetNativeDesc()
+    {
+        const GraphicsBufferDesc& bufferDesc = GetDesc();
+
+        D3D12_RESOURCE_DESC desc;
+        MemoryManager::Zero(&desc);
+
+        switch (bufferDesc.resourceType)
+        {
+            case RT_Texture:
+            {
+                desc = CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_R8G8B8A8_UNORM, bufferDesc.texture.resolution.x, bufferDesc.texture.resolution.y);
+                if (bufferDesc.texture.renderTarget)
+                {
+                    desc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
+                }
+                if (bufferDesc.texture.shaderResource)
+                {
+                    desc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+                }
+                break;
+            }
+
+            case RT_ConstantBuffer:
+            {
+                desc = CD3DX12_RESOURCE_DESC::Buffer(bufferDesc.dataSize);
+                break;
+            }
+            case RT_VertexBuffer:
+            {
+                desc = CD3DX12_RESOURCE_DESC::Buffer(bufferDesc.dataSize);
+                break;
+            }
+            case RT_IndexBuffer:
+            {
+                desc = CD3DX12_RESOURCE_DESC::Buffer(bufferDesc.dataSize);
+                break;
+            }
+        }
+
+        return desc;
+    }
+}
---
+++ Source/Engine/Graphics/GraphicsD3D12/Source/GraphicsBufferD3D12.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <GraphicsAdapter.hpp>
+#include <d3d12.h>
+#include <dxgi.h>
+
+namespace Faro
+{
+    class IGraphicsBufferD3D12 : public GraphicsBuffer
+    {
+    public:
+        void Destroy() override;
+
+    protected:
+        uint8* cpuAddress = nullptr;
+        ID3D12Resource* gpuResource = nullptr;
+    };
+
+    class GraphicsBufferUploadD3D12 : public IGraphicsBufferD3D12
+    {
+    public:
+        void Init() override;
+    };
+
+    class GraphicsBufferRemoteD3D12 : public IGraphicsBufferD3D12
+    {
+    public:
+        void Init() override;
+
+    private:
+        D3D12_RESOURCE_DESC GetNativeDesc();
+    };
+}
