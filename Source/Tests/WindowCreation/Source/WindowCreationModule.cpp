d6de2e4c45c69d97885bad1df699fc191585d5df
R00tDroid <info@rootdroid.nl>
2022-10-20 21:27:40 +0200

    Create and destroy buffers

--- Source/Tests/WindowCreation/Source/WindowCreationModule.cpp
+++ Source/Tests/WindowCreation/Source/WindowCreationModule.cpp
@@ -1,6 +1,8 @@
 #include "WindowCreationModule.hpp"
 #include <Log.hpp>
+#include <GraphicsInterface.hpp>
 
+#include "Memory/MemoryManager.hpp"
 
 namespace Faro
 {
@@ -8,6 +10,20 @@ namespace Faro
     {
         window = GPlatform->CreateWindow();
         window->SetTitle("Test window");
+
+        Array<GraphicsAdapterDesc> adapters = GGraphics->GetAdapters();
+        GraphicsAdapter* adapter = GGraphics->CreateAdapter(adapters[0]);
+
+        GraphicsCommandList* commandList = adapter->CreateCommandList();
+
+        GraphicsBufferDesc bufferDesc = GraphicsBufferDesc::Texture2D({ 400, 300 }, true, false, false);
+        GraphicsBuffer* uploadBuffer = adapter->CreateBuffer(GraphicsBufferType::BT_Upload, bufferDesc);
+        GraphicsBuffer* remoteBuffer = adapter->CreateBuffer(GraphicsBufferType::BT_Remote, bufferDesc);
+
+        MemoryManager::SafeDelete(remoteBuffer);
+        MemoryManager::SafeDelete(uploadBuffer);
+        MemoryManager::SafeDelete(commandList);
+        MemoryManager::SafeDelete(adapter);
     }
 
     void WindowCreationModule::Unload()
