3e67b66661952923033240e84979d6153fcab96f
R00tDroid <info@rootdroid.nl>
2022-10-22 13:53:14 +0200

    Document graphics buffer

--- Source/Engine/Graphics/GraphicsBase/Source/GraphicsBuffer.hpp
+++ Source/Engine/Graphics/GraphicsBase/Source/GraphicsBuffer.hpp
@@ -32,6 +32,7 @@ namespace Faro
         RT_IndexBuffer //< The data should be treated as mesh indices.
     };
 
+    /// @brief Description of a graphics buffer
     struct GraphicsBufferDesc 
     {
         GraphicsResourceType resourceType;
@@ -68,18 +69,35 @@ namespace Faro
         }
     };
 
+    /// @brief A buffer storing graphics related data. Created by GraphicsAdapter::CreateBuffer.
     class GraphicsBuffer : public IGraphicsAdapterChild
     {
     public:
+        /**
+         * @brief Get the description of this buffer.
+         * @return const GraphicsBufferDesc& Buffer description
+         */
         const GraphicsBufferDesc& GetDesc();
 
         void Init(GraphicsAdapter* adapter, GraphicsBufferDesc desc);     
-        
+
+        /**
+         * @brief Prepare the given data so it can be uploaded to the graphics adapter. This buffer **must** be created as BT_Upload!
+         * @param data Pointer to the data to upload
+         */
         virtual void Upload(uint8* data) = 0;
 
+        /**
+         * @brief Get the current resource state of this buffer.
+         * @return GraphicsResourceState Current resource state
+         */
         GraphicsResourceState GetResourceState();
 
     protected:
+        /**
+         * @brief Set the resource state flag for internal referencing.
+         * @param state New resource state
+         */
         void SetResourceState(GraphicsResourceState state);
 
     private:
