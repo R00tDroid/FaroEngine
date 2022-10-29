efe63dfd5fba6093a671b3b9e2d933469e046277
R00tDroid <info@rootdroid.nl>
2022-10-29 16:05:36 +0200

    Print help info

--- Tools/ShaderTranspiler/Source/Main.cpp
+++ Tools/ShaderTranspiler/Source/Main.cpp
@@ -11,6 +11,12 @@ int main(int argc, char** argv)
 {
     ParameterList parameters(argc, argv);
 
+    if (parameters.Contains("help"))
+    {
+        PrintHelp();
+        return 0;
+    }
+
     Utility::PrintLine("No tasks specified");
     return -1;
 }
