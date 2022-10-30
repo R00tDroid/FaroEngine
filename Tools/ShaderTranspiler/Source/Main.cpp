d3bafd675b474697bd8982471451f061152a542f
R00tDroid <info@rootdroid.nl>
2022-10-30 20:32:32 +0100

    Parse and check shader paths

--- Tools/ShaderTranspiler/Source/Main.cpp
+++ Tools/ShaderTranspiler/Source/Main.cpp
@@ -20,6 +20,35 @@ int main(int argc, char** argv)
         return 0;
     }
 
+    std::vector<std::filesystem::path> shaderFiles;
+    if (parameters.Contains("shader"))
+    {
+        if (parameters.CountArguments("shader") == 0)
+        {
+            Utility::PrintLine("-shader requires at least one path");
+            return -1;
+        }
+
+        bool anyFailed = false;
+        for (std::string shaderPathString : parameters.GetArguments("shader"))
+        {
+            std::filesystem::path shaderPath = std::filesystem::absolute(shaderPathString);
+            shaderPath = weakly_canonical(shaderPath);
+            shaderPath.make_preferred();
+
+            if (!std::filesystem::exists(shaderPath))
+            {
+                Utility::PrintLine("Could not find input file: " + shaderPath.string());
+                anyFailed = true;
+                continue;
+            }
+
+            shaderFiles.push_back(shaderPath);
+        }
+
+        if (anyFailed) return -1;
+    }
+
     std::filesystem::path dxcExe;
     const std::vector<WindowsKit>& windowsKits = GetWindowsKits();
     for (const WindowsKit& windowsKit : windowsKits)
