f0f83b6ff48d2d3daeab40134a5ca08c9389c1be
R00tDroid <info@rootdroid.nl>
2022-10-31 16:58:36 +0100

    Use shared command execution

--- Tools/BuildToolGUI/Source/Console.cpp
+++ Tools/BuildToolGUI/Source/Console.cpp
@@ -2,7 +2,7 @@
 #include "Window.hpp"
 #include <array>
 #include <mutex>
-#include <thread>
+#include "Command.hpp"
 
 std::filesystem::path Console::buildTool;
 
@@ -29,93 +29,40 @@ void Console::ExecuteCommand(std::wstring command, std::wstring platform, std::w
     InvokeCommand(L"\"" + buildTool.wstring() + L"\" " + command + L" -project \"" + project + L"\" -platform " + platform + L" -" + config);
 }
 
-std::string Convert(std::wstring string)
-{
-    int stringLength = WideCharToMultiByte(CP_ACP, 0, string.c_str(), (int)string.length(), nullptr, 0, nullptr, nullptr);
-    std::string str(stringLength, 0);
-    WideCharToMultiByte(CP_ACP, 0, string.c_str(), -1, &str[0], stringLength, nullptr, nullptr);
-    return str;
-}
-
-std::wstring Convert(std::string string)
-{
-    int stringLength = MultiByteToWideChar(CP_ACP, 0, string.c_str(), (int)string.length(), nullptr, 0);
-    std::wstring wstr(stringLength, 0);
-    MultiByteToWideChar(CP_ACP, 0, string.c_str(), (int)string.length(), &wstr[0], stringLength);
-    return wstr;
-}
-
 std::mutex invokeLock;
 
 void Console::InvokeCommand(std::wstring command)
 {
-    std::thread thread([command]()
+    Utility::ExecuteCommandAsync(Utility::Convert(command),
+    []()
     {
         invokeLock.lock();
-
-        HANDLE stdOutWrite = nullptr;
-        HANDLE stdOutRead = nullptr;
-
-        SECURITY_ATTRIBUTES securityDesc;
-        securityDesc.nLength = sizeof(SECURITY_ATTRIBUTES);
-        securityDesc.bInheritHandle = true;
-        securityDesc.lpSecurityDescriptor = nullptr;
-        CreatePipe(&stdOutWrite, &stdOutRead, &securityDesc, 0);
-        SetHandleInformation(stdOutWrite, HANDLE_FLAG_INHERIT, 0);
-
-        PROCESS_INFORMATION processInfo;
-        ZeroMemory(&processInfo, sizeof(PROCESS_INFORMATION));
-
-        STARTUPINFOW startupInfo;
-        ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
-        startupInfo.cb = sizeof(STARTUPINFO);
-        startupInfo.hStdOutput = stdOutRead;
-        startupInfo.dwFlags |= STARTF_USESTDHANDLES;
-
-        if (!CreateProcessW(nullptr, const_cast<wchar*>((command + L" 2>&1").c_str()), nullptr, nullptr, true, CREATE_NO_WINDOW, nullptr, nullptr, &startupInfo, &processInfo))
+    },
+    [](std::string logString)
+    {
+        Append(Utility::Convert(logString));
+    },
+    [](bool commandSuccess, int commandResult)
+    {
+        if (!commandSuccess)
         {
-            CloseHandle(processInfo.hProcess);
-            CloseHandle(processInfo.hThread);
-
             AppendLine(L"Failed to invoke command");
-            LockConsole(false);
-            invokeLock.unlock();
-            return;
-        }
-
-        CloseHandle(stdOutRead);
-        
-        while (true)
-        {
-            DWORD readSize;
-            CHAR logBuffer[128];
-            if (!ReadFile(stdOutWrite, logBuffer, 128, &readSize, nullptr)) break;
-            if (readSize == 0) break;
-            std::string logString(logBuffer, readSize);
-            Append(Convert(logString));
-        }
-
-        WaitForSingleObject(processInfo.hProcess, INFINITE);
-        DWORD exitCode;
-        GetExitCodeProcess(processInfo.hProcess, &exitCode);
-
-        CloseHandle(processInfo.hProcess);
-        CloseHandle(processInfo.hThread);
-
-        if (exitCode == 0)
-        {
-            AppendLine(L"Task completed successfully");
         }
         else
         {
-            AppendLine(L"Task failed with status code: " + std::to_wstring((int)exitCode));
+            if (commandResult == 0)
+            {
+                AppendLine(L"Task completed successfully");
+            }
+            else
+            {
+                AppendLine(L"Task failed with status code: " + std::to_wstring((int)commandResult));
+            }
         }
 
         LockConsole(false);
         invokeLock.unlock();
     });
-
-    thread.detach();
 }
 
 std::mutex consoleLock;
