8ed866ff5e57b0db461a279228e2b1f39ad7b2ae
R00tDroid <info@rootdroid.nl>
2022-11-05 19:20:49 +0100

    Implement linux version of ReadEnvVariable

--- Tools/Shared/VulkanSDKInfo/VulkanSDKInfo.cpp
+++ Tools/Shared/VulkanSDKInfo/VulkanSDKInfo.cpp
@@ -5,18 +5,20 @@
 
 bool ReadEnvVariable(std::string variableName, std::string& value)
 {
+#ifdef WIN32
     size_t size = 0;
     if (getenv_s(&size, nullptr, 0, variableName.c_str()) != 0)
     {
         return false;
     }
-
     value.resize(size);
-
     if (getenv_s(&size, value.data(), size, variableName.c_str()) != 0)
     {
         return false;
     }
+#else
+    value = std::string(getenv(variableName.c_str()));
+#endif
 
     // Remove null-terminator at end of string
     if (value.back() == '\0')
