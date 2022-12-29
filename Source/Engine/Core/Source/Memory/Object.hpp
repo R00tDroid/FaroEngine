0617886f97f8f71ce905f8b7d1a965791d46bb9b
R00tDroid <info@rootdroid.nl>
2022-12-29 18:36:42 +0100

    Document IObject

--- Source/Engine/Core/Source/Memory/Object.hpp
+++ Source/Engine/Core/Source/Memory/Object.hpp
@@ -2,14 +2,19 @@
 
 namespace Faro
 {
+    /// @brief Base class for dynamically allocated objects.
     class IObject
     {
     public:
         virtual ~IObject() = default;
 
+        /// @brief Run the initialization logic of this object. Should be overriden by deriving classes.
         virtual void Init();
 
-        // @warning Must be called last
+        /**
+         * @brief Run the destruction logic of this class. This also frees its memory.
+         * @warning When overriding, the super call must be performed **last**
+         */
         virtual void Destroy();
     };
 }
