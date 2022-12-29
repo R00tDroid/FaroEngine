dbf92401bbfe6c402dd29241af67e012a33b1cb2
R00tDroid <info@rootdroid.nl>
2022-12-29 10:16:31 +0100

    Describe packaging layout

--- Tools/ShaderTranspiler/Source/Shader.hpp
+++ Tools/ShaderTranspiler/Source/Shader.hpp
@@ -17,3 +17,25 @@ struct Shader
     ShaderBinary dxil;
     ShaderBinary spirV;
 };
+
+#define ShaderBinaryType_DXIL 0x01
+#define ShaderBinaryType_SPRIV 0x02
+
+#define ShaderStageType_Vertex 0x01
+#define ShaderStageType_Geometry 0x02
+#define ShaderStageType_Pixel 0x03
+
+/* --Packaging layout--
+ *
+ * Header
+ *   File identifier, 2x uint8: 0x46 0x53
+ *   File version, uint16
+ *
+ * Shader
+ *   ShaderBinaryType, uint8
+ *
+ *   Shader stage
+ *     ShaderStageType, uint8
+ *     ShaderStageSize, uint32
+ *     ShaderBinaryData, ShaderStageSize-x uint8
+ */
