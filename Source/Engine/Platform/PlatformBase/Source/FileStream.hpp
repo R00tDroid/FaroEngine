e6ffc01ffc46af94db25598baed9f10e6eee3fb1
R00tDroid <info@rootdroid.nl>
2022-12-30 16:25:16 +0100

    Document file streams

--- Source/Engine/Platform/PlatformBase/Source/FileStream.hpp
+++ Source/Engine/Platform/PlatformBase/Source/FileStream.hpp
@@ -3,16 +3,22 @@
 
 namespace Faro
 {
+    /// @brief Mode in which to open a file
     enum EFileMode
     {
-        FO_Overwrite,
-        FO_Append,
-        FO_Read
+        FO_Overwrite, ///< Open a file for writing. Will overwrite any existing data.
+        FO_Append, ///< Open a file for writing. Will append to any existing data.
+        FO_Read ///< Open a file for reading.
     };
 
     class FileStream : public DataStream
     {
     public:
+        /**
+         * @brief Check if the file stream was successfully opened.
+         * @return true The stream is open
+         * @return false The stream is not open
+         */
         virtual bool IsOpen() = 0;
     };
 }
