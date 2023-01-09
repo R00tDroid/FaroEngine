4053b309eff1feb23a572c59afe83e40c593c018
R00tDroid <info@rootdroid.nl>
2023-01-09 21:13:03 +0100

    Setup claim logic

--- Source/Engine/Resources/Source/Resource.cpp
+++ Source/Engine/Resources/Source/Resource.cpp
@@ -1,5 +1,26 @@
 #include "Resource.hpp"
+#include <Assert.hpp>
 
 namespace Faro
 {
+    void IResource::ClaimResource()
+    {
+        claims++;
+    }
+
+    void IResource::ReleaseResource()
+    {
+        Debug_Assert(claims > 0, "Mismatched claim and release");
+        claims--;
+    }
+
+    bool IResource::IsResourceClaimed()
+    {
+        return claims > 0;
+    }
+
+    ResourceState IResource::GetResourceState()
+    {
+        return state.Get();
+    }
 }
--- Source/Engine/Resources/Source/Resource.hpp
+++ Source/Engine/Resources/Source/Resource.hpp
@@ -1,8 +1,33 @@
 #pragma once
+#include <Primitives.hpp>
+#include <ThreadSafety.hpp>
+
+enum ResourceState
+{
+    RS_Unknown,
+    RS_Queued,
+    RS_Loading,
+    RS_Available,
+    RS_PendingDestroy
+};
 
 namespace Faro
 {
     class IResource
     {
+    public:
+        void ClaimResource();
+        void ReleaseResource();
+        bool IsResourceClaimed();
+
+        ResourceState GetResourceState();
+
+    protected:
+        virtual void InitResource() = 0;
+        virtual void DestroyResource() = 0;
+
+    private:
+        uint32 claims = 0;
+        ThreadSafe<ResourceState> state = RS_Unknown;
     };
 }
