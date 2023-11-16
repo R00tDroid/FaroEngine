61b59fb77adaa267d53105ba6a1d8b33dc4bab08
R00tDroid <info@rootdroid.nl>
2023-11-16 21:49:23 +0100

    Check cached project

--- Tools/BuildTool/Source/Tests/TestProjectManifest.cpp
+++ Tools/BuildTool/Source/Tests/TestProjectManifest.cpp
@@ -25,4 +25,23 @@ TEST(ProjectManifest, Parse)
     EXPECT_STREQ(manifest->projectName.c_str(), "Test Project");
     EXPECT_EQ(manifest->modulesPaths.size(), 3);
 }
+
+TEST(ProjectManifest, LoadCached)
+{
+    std::filesystem::path project = GetTestProjectRoot();
+
+    {
+        ProjectManifest* manifest = ProjectManifest::Parse(project);
+        EXPECT_NE(manifest, nullptr);
+        EXPECT_STREQ(manifest->projectName.c_str(), "Test Project");
+        EXPECT_EQ(manifest->modulesPaths.size(), 3);
+    }
+
+    {
+        ProjectManifest* manifest = ProjectManifest::LoadFromCache(project);
+        EXPECT_NE(manifest, nullptr);
+        EXPECT_STREQ(manifest->projectName.c_str(), "Test Project");
+        EXPECT_EQ(manifest->modulesPaths.size(), 3);
+    }
+}
 #endif
