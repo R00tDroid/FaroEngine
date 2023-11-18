6638b297585531c1551ef20402aeef5ff3d58b81
R00tDroid <info@rootdroid.nl>
2023-11-18 11:08:55 +0100

    Check parsed and loaded manifests

--- Tools/BuildTool/Source/Tests/TestModuleManifest.cpp
+++ Tools/BuildTool/Source/Tests/TestModuleManifest.cpp
@@ -51,25 +51,24 @@ TEST(ModuleManifest, LoadCached)
     std::filesystem::path modulePath = project->modulesPaths[0];
     EXPECT_STREQ(modulePath.filename().string().c_str(), "ModuleA.module.json");
 
-    {
-        ModuleManifest* manifest = ModuleManifest::LoadFromCache(modulePath, project);
-        EXPECT_NE(manifest, nullptr);
-    }
+    ModuleManifest* parsedManifest = ModuleManifest::Parse(modulePath, project);
+    EXPECT_NE(parsedManifest, nullptr);
 
-    {
-        ModuleManifest* manifest = ModuleManifest::LoadFromCache(modulePath, project);
-        EXPECT_NE(manifest, nullptr);
-        EXPECT_EQ(manifest->project, project);
-        EXPECT_EQ(manifest->sourceFiles.size(), 2);
-        EXPECT_EQ(manifest->publicIncludes.size(), 1);
-        EXPECT_EQ(manifest->privateIncludes.size(), 0);
-        EXPECT_EQ(manifest->publicDefines.size(), 0);
-        EXPECT_EQ(manifest->privateDefines.size(), 0);
-        EXPECT_EQ(manifest->linkingLibraries.size(), 0);
-        EXPECT_EQ(manifest->moduleDependencies.size(), 0);
-        EXPECT_EQ(manifest->type, MT_Library);
-        EXPECT_STRNE(manifest->uuid.c_str(), "");
-    }
+    ModuleManifest* loadedManifest = ModuleManifest::LoadFromCache(modulePath, project);
+    EXPECT_NE(loadedManifest, nullptr);
+
+    EXPECT_NE(loadedManifest, parsedManifest);
+
+    EXPECT_EQ(loadedManifest->project, parsedManifest->project);
+    EXPECT_EQ(loadedManifest->sourceFiles.size(), parsedManifest->sourceFiles.size());
+    EXPECT_EQ(loadedManifest->publicIncludes.size(), parsedManifest->publicIncludes.size());
+    EXPECT_EQ(loadedManifest->privateIncludes.size(), parsedManifest->privateIncludes.size());
+    EXPECT_EQ(loadedManifest->publicDefines.size(), parsedManifest->publicDefines.size());
+    EXPECT_EQ(loadedManifest->privateDefines.size(), parsedManifest->privateDefines.size());
+    EXPECT_EQ(loadedManifest->linkingLibraries.size(), parsedManifest->linkingLibraries.size());
+    EXPECT_EQ(loadedManifest->moduleDependencies.size(), parsedManifest->moduleDependencies.size());
+    EXPECT_EQ(loadedManifest->type, MT_Library);
+    EXPECT_STRNE(loadedManifest->uuid.c_str(), "");
 }
 
 #endif
--- Tools/BuildTool/Source/Tests/TestProjectManifest.cpp
+++ Tools/BuildTool/Source/Tests/TestProjectManifest.cpp
@@ -23,18 +23,16 @@ TEST(ProjectManifest, LoadCached)
 {
     std::filesystem::path project = GetTestProjectManifest();
 
-    {
-        ProjectManifest* manifest = ProjectManifest::Parse(project);
-        EXPECT_NE(manifest, nullptr);
-        EXPECT_STREQ(manifest->projectName.c_str(), "Test Project");
-        EXPECT_EQ(manifest->modulesPaths.size(), 3);
-    }
 
-    {
-        ProjectManifest* manifest = ProjectManifest::LoadFromCache(project);
-        EXPECT_NE(manifest, nullptr);
-        EXPECT_STREQ(manifest->projectName.c_str(), "Test Project");
-        EXPECT_EQ(manifest->modulesPaths.size(), 3);
-    }
+    ProjectManifest* parsedManifest = ProjectManifest::Parse(project);
+    EXPECT_NE(parsedManifest, nullptr);
+
+    ProjectManifest* loadedManifest = ProjectManifest::LoadFromCache(project);
+    EXPECT_NE(loadedManifest, nullptr);
+
+    EXPECT_NE(loadedManifest, parsedManifest);
+
+    EXPECT_STREQ(loadedManifest->projectName.c_str(), parsedManifest->projectName.c_str());
+    EXPECT_EQ(loadedManifest->modulesPaths.size(), parsedManifest->modulesPaths.size());
 }
 #endif
