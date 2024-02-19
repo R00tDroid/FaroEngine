537b262a10309c632dd9944a9227501e30b9b50e
R00tDroid <info@rootdroid.nl>
2024-02-19 20:17:06 +0100

    Implement sync points

--- Source/Engine/Graphics/GraphicsBase/Source/GraphicsAdapter.cpp
+++ Source/Engine/Graphics/GraphicsBase/Source/GraphicsAdapter.cpp
@@ -2,4 +2,10 @@
 
 namespace Faro
 {
+    GraphicsSyncPoint* GraphicsAdapter::CreateSyncPoint()
+    {
+        GraphicsSyncPoint* syncPoint = MemoryManager::New<GraphicsSyncPoint>();
+        syncPoint->Init(this);
+        return syncPoint;
+    }
 }
