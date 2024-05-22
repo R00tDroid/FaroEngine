12297b561b94388e2c7f3f5c0995478c4e8ed488
R00tdroid <info@rootdroid.nl>
2024-05-22 23:02:27 +0200

    Add web debug break

--- Source/Engine/Core/Source/Assert.hpp
+++ Source/Engine/Core/Source/Assert.hpp
@@ -6,6 +6,7 @@
 #ifdef FARO_DEBUG // Define internal debugging macros
 
     #ifdef FARO_OS_WINDOWS
+
     #include <sstream>
     #include <MinWindows.hpp>
     #define DebugOutputMessage(message)           \
@@ -16,7 +17,18 @@
         OutputDebugStringA(stream.str().c_str()); \
     }
     #define Debug_Break() __debugbreak()
+
+    #elif defined(FARO_OS_WEB)
+
+    #define DebugOutputMessage(message)  \
+        {                                    \
+            std::cerr << message;            \
+        }
+
+    #define Debug_Break()
+
     #else
+
     #define DebugOutputMessage(message)  \
     {                                    \
         std::cerr << message;            \
