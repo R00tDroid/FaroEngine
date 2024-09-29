526eb43f728cab90f47f180adff4f63d49528875
R00tDroid <R00tDroid@users.noreply.github.com>
2024-09-29 20:14:45 +0000

    deploy: 503e7bf1d8b463fe157588d2eb67092e3f108f53

--- search/files_4.js
+++ search/files_4.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['filestream_2ehpp_0',['FileStream.hpp',['../FileStream_8hpp.html',1,'']]],
-  ['filestreamweb_2ecpp_1',['FileStreamWeb.cpp',['../FileStreamWeb_8cpp.html',1,'']]],
-  ['filestreamweb_2ehpp_2',['FileStreamWeb.hpp',['../FileStreamWeb_8hpp.html',1,'']]],
-  ['filestreamwindows_2ecpp_3',['FileStreamWindows.cpp',['../FileStreamWindows_8cpp.html',1,'']]],
-  ['filestreamwindows_2ehpp_4',['FileStreamWindows.hpp',['../FileStreamWindows_8hpp.html',1,'']]]
+  ['filestreamandroid_2ecpp_1',['FileStreamAndroid.cpp',['../FileStreamAndroid_8cpp.html',1,'']]],
+  ['filestreamandroid_2ehpp_2',['FileStreamAndroid.hpp',['../FileStreamAndroid_8hpp.html',1,'']]],
+  ['filestreamweb_2ecpp_3',['FileStreamWeb.cpp',['../FileStreamWeb_8cpp.html',1,'']]],
+  ['filestreamweb_2ehpp_4',['FileStreamWeb.hpp',['../FileStreamWeb_8hpp.html',1,'']]],
+  ['filestreamwindows_2ecpp_5',['FileStreamWindows.cpp',['../FileStreamWindows_8cpp.html',1,'']]],
+  ['filestreamwindows_2ehpp_6',['FileStreamWindows.hpp',['../FileStreamWindows_8hpp.html',1,'']]]
 ];
--- search/files_f.js
+++ search/files_f.js
@@ -2,8 +2,10 @@ var searchData=
 [
   ['window_2ecpp_0',['Window.cpp',['../Window_8cpp.html',1,'']]],
   ['window_2ehpp_1',['Window.hpp',['../Window_8hpp.html',1,'']]],
-  ['windowweb_2ecpp_2',['WindowWeb.cpp',['../WindowWeb_8cpp.html',1,'']]],
-  ['windowweb_2ehpp_3',['WindowWeb.hpp',['../WindowWeb_8hpp.html',1,'']]],
-  ['windowwindows_2ecpp_4',['WindowWindows.cpp',['../WindowWindows_8cpp.html',1,'']]],
-  ['windowwindows_2ehpp_5',['WindowWindows.hpp',['../WindowWindows_8hpp.html',1,'']]]
+  ['windowandroid_2ecpp_2',['WindowAndroid.cpp',['../WindowAndroid_8cpp.html',1,'']]],
+  ['windowandroid_2ehpp_3',['WindowAndroid.hpp',['../WindowAndroid_8hpp.html',1,'']]],
+  ['windowweb_2ecpp_4',['WindowWeb.cpp',['../WindowWeb_8cpp.html',1,'']]],
+  ['windowweb_2ehpp_5',['WindowWeb.hpp',['../WindowWeb_8hpp.html',1,'']]],
+  ['windowwindows_2ecpp_6',['WindowWindows.cpp',['../WindowWindows_8cpp.html',1,'']]],
+  ['windowwindows_2ehpp_7',['WindowWindows.hpp',['../WindowWindows_8hpp.html',1,'']]]
 ];
