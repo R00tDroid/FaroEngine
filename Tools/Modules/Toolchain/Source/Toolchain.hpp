446b1df5e311926c4ffdd4f00674e1ed110d0700
R00tDroid <info@rootdroid.nl>
2024-12-31 18:01:07 +0100

    Expose toolchain list

--- Tools/Modules/Toolchain/Source/Toolchain.hpp
+++ Tools/Modules/Toolchain/Source/Toolchain.hpp
@@ -1,7 +1,12 @@
 #pragma once
+#include <vector>
 #include "FaroToolchainExports.generated.h"
 
 class FaroToolchainExports Toolchain
 {
 public:
+    inline static std::vector<Toolchain*> getToolchains()
+    {
+        return {}; //TODO Instantiate toolchains
+    }
 };
