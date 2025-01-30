da1cff03c896ae6fbdf896a408dc85740b7023ae
R00tDroid <info@rootdroid.nl>
2025-01-30 17:40:55 +0100

    Load and configure project

--- Tools/Applications/FaroCLI/Source/Main.cpp
+++ Tools/Applications/FaroCLI/Source/Main.cpp
@@ -1,6 +1,7 @@
 #include "Parameters.hpp"
 #include "Utility.hpp"
 #include "Commandline.hpp"
+#include "ProjectInfo.hpp"
 
 int main(int argc, char** argv)
 {
@@ -17,6 +18,28 @@ int main(int argc, char** argv)
     if (!parseParameters(parameters, taskInfo))
     {
         return -1;
+    }
+
+    ProjectManifest project(taskInfo.manifest.string().c_str());
+
+    if (taskInfo.runConfigure)
+    {
+        if (!project.configure())
+        {
+            Utility::PrintLine("Error while configuring project");
+            return -1;
+        }
+    }
+    else if (taskInfo.runProject ||
+             taskInfo.runBuild ||
+             taskInfo.runClean ||
+             taskInfo.runDeploy)
+    {
+        if (!project.load())
+        {
+            Utility::PrintLine("Error while loading project");
+            return -1;
+        }
     }
 
     Utility::PrintLine("No tasks specified");
