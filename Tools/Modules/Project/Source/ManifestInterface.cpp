93e23e1108a788c7f43a4291e41847bd777fe023
R00tDroid <info@rootdroid.nl>
2025-01-30 17:45:16 +0100

    Fix manifests paths

--- Tools/Modules/Project/Source/ManifestInterface.cpp
+++ Tools/Modules/Project/Source/ManifestInterface.cpp
@@ -4,27 +4,32 @@
 
 struct IManifest::Impl
 {
-    std::filesystem::path manifestPath = "";
-    std::filesystem::path manifestDirectory = "";
-    std::filesystem::path faroDirectory = "";
-    std::filesystem::path cacheDirectory = "";
+    std::string manifestPath = "";
+    std::string manifestDirectory = "";
+    std::string faroDirectory = "";
+    std::string cacheDirectory = "";
 };
 
 IManifest::IManifest(const char* manifestLocation)
 {
     impl = new Impl();
 
-    impl->manifestPath = manifestLocation;
+    std::filesystem::path manifestPath(manifestLocation);
+    manifestPath.make_preferred();
+    manifestPath = weakly_canonical(manifestPath);
+    impl->manifestPath = manifestPath.string();
 
-    impl->manifestDirectory = impl->manifestPath.parent_path();
-    impl->faroDirectory = impl->manifestDirectory / ".Faro";
-    impl->cacheDirectory = impl->faroDirectory / "Cache";
+    std::filesystem::path manifestDirectory = manifestPath.parent_path();
+    impl->manifestDirectory = manifestDirectory.string();
+    std::filesystem::path faroDirectory = manifestDirectory / ".Faro";
+    impl->faroDirectory = faroDirectory.string();
+    impl->cacheDirectory = (faroDirectory / "Cache").string();
 
     if (std::filesystem::exists(impl->manifestPath))
     {
-        Utility::EnsureDirectory(impl->faroDirectory.string().c_str());
-        Utility::HideFolder(impl->faroDirectory.string().c_str());
-        Utility::EnsureDirectory(impl->cacheDirectory.string().c_str());
+        Utility::EnsureDirectory(impl->faroDirectory.c_str());
+        Utility::HideFolder(impl->faroDirectory.c_str());
+        Utility::EnsureDirectory(impl->cacheDirectory.c_str());
     }
 }
 
@@ -35,20 +40,20 @@ IManifest::~IManifest()
 
 const char* IManifest::manifestPath() const
 {
-    return impl->manifestPath.string().c_str();
+    return impl->manifestPath.c_str();
 }
 
 const char* IManifest::manifestDirectory() const
 {
-    return impl->manifestDirectory.string().c_str();
+    return impl->manifestDirectory.c_str();
 }
 
 const char* IManifest::faroDirectory() const
 {
-    return impl->faroDirectory.string().c_str();
+    return impl->faroDirectory.c_str();
 }
 
 const char* IManifest::cacheDirectory() const
 {
-    return impl->cacheDirectory.string().c_str();
+    return impl->cacheDirectory.c_str();
 }
