2803bc7ecadff5c2918ef2c86822514f9caa102a
R00tDroid <info@rootdroid.nl>
2025-02-18 22:09:19 +0100

    Get combined info

--- Tools/Modules/Project/Source/ModuleInfo.cpp
+++ Tools/Modules/Project/Source/ModuleInfo.cpp
@@ -11,28 +11,52 @@ struct FolderMount::Impl
     std::string mountPoint;
 };
 
+#define CombineImpls(name) std::vector<std::string> _##name; const std::vector<std::string>& name() { size_t size = module.name.size() + setup.name.size(); if (_##name.size() != size) { _##name.clear(); _##name.reserve(size); _##name.insert(_##name.end(), module.name.begin(), module.name.end()); _##name.insert(_##name.end(), setup.name.begin(), setup.name.end()); } return _##name; }
+
 struct ModuleManifest::Impl
 {
-    std::string name;
-    std::string solutionDirectory;
+    struct ModuleImpl
+    {
+        std::string name;
+        std::string solutionDirectory;
 
-    std::vector<std::string> linkingLibraries;
-    std::vector<std::string> publicDefines;
-    std::vector<std::string> privateDefines;
+        std::vector<std::string> linkingLibraries;
+        std::vector<std::string> publicDefines;
+        std::vector<std::string> privateDefines;
 
-    ModuleType type = MT_Library;
+        ModuleType type = MT_Library;
 
-    std::vector<std::string> moduleDependencyPaths; //TODO Resolve path to module
-    std::vector<ModuleManifest*> moduleDependencies;
+        std::vector<std::string> moduleDependencyPaths; //TODO Resolve path to module
+        std::vector<ModuleManifest*> moduleDependencies;
 
-    std::vector<std::string> privateIncludes;
-    std::vector<std::string> publicIncludes;
+        std::vector<std::string> privateIncludes;
+        std::vector<std::string> publicIncludes;
 
-    std::vector<std::string> sourceFiles;
+        std::vector<std::string> sourceFiles;
 
-    std::vector<FolderMount> folderMounts;
+        std::vector<FolderMount> folderMounts;
 
-    std::string uuid = "";
+        std::string uuid = "";
+    };
+    ModuleImpl module;
+
+    struct SetupImpl
+    {
+        std::vector<std::string> linkingLibraries;
+        std::vector<std::string> publicDefines;
+        std::vector<std::string> privateDefines;
+
+        std::vector<std::string> privateIncludes;
+        std::vector<std::string> publicIncludes;
+    };
+    SetupImpl setup;
+
+    CombineImpls(linkingLibraries);
+    CombineImpls(publicDefines);
+    CombineImpls(privateDefines);
+
+    CombineImpls(privateIncludes);
+    CombineImpls(publicIncludes);
 };
 
 FolderMount::FolderMount(const char* location, const char* mountPoint)
@@ -74,37 +98,37 @@ ModuleManifest::~ModuleManifest()
 
 const char* ModuleManifest::name() const
 {
-    return impl->name.c_str();
+    return impl->module.name.c_str();
 }
 
 void ModuleManifest::setName(const char* name)
 {
-    impl->name = name;
+    impl->module.name = name;
 }
 
 const char* ModuleManifest::solutionLocation() const
 {
-    return impl->solutionDirectory.c_str();
+    return impl->module.solutionDirectory.c_str();
 }
 
 void ModuleManifest::setSolutionLocation(const char* location)
 {
-    impl->solutionDirectory = location;
+    impl->module.solutionDirectory = location;
 }
 
 void ModuleManifest::addLinkerLibrary(const char* library) const
 {
-    impl->linkingLibraries.push_back(library);
+    impl->module.linkingLibraries.push_back(library);
 }
 
 void ModuleManifest::setModuleType(ModuleType type) const
 {
-    impl->type = type;
+    impl->module.type = type;
 }
 
 void ModuleManifest::addDependency(const char* dependency)
 {
-    impl->moduleDependencyPaths.push_back(dependency);
+    impl->module.moduleDependencyPaths.push_back(dependency);
 }
 
 void ModuleManifest::addIncludePath(AccessDomain type, const char* path) const
@@ -113,12 +137,12 @@ void ModuleManifest::addIncludePath(AccessDomain type, const char* path) const
     {
         case AD_Private:
         {
-            impl->privateIncludes.push_back(path);
+            impl->module.privateIncludes.push_back(path);
             break;
         }
         case AD_Public:
         {
-            impl->publicIncludes.push_back(path);
+            impl->module.publicIncludes.push_back(path);
             break;
         }
     }
@@ -126,7 +150,7 @@ void ModuleManifest::addIncludePath(AccessDomain type, const char* path) const
 
 void ModuleManifest::addSource(const char* path) const
 {
-    impl->sourceFiles.push_back(path);
+    impl->module.sourceFiles.push_back(path);
 }
 
 ProjectManifest* ModuleManifest::project() const
@@ -138,9 +162,9 @@ unsigned int ModuleManifest::defines(AccessDomain type) const
 {
     switch (type) {
     case AD_Private:
-        return static_cast<unsigned int>(impl->privateDefines.size());
+        return static_cast<unsigned int>(impl->privateDefines().size());
     case AD_Public:
-        return static_cast<unsigned int>(impl->publicDefines.size());
+        return static_cast<unsigned int>(impl->publicDefines().size());
     }
     return 0;
 }
@@ -149,45 +173,45 @@ const char* ModuleManifest::define(AccessDomain type, unsigned int index) const
 {
     switch (type) {
     case AD_Private:
-        return impl->privateDefines[index].c_str();
+        return impl->privateDefines()[index].c_str();
     case AD_Public:
-        return impl->publicDefines[index].c_str();
+        return impl->publicDefines()[index].c_str();
     }
     return nullptr;
 }
 
 unsigned int ModuleManifest::linkerLibraries() const
 {
-    return static_cast<unsigned int>(impl->linkingLibraries.size());
+    return static_cast<unsigned int>(impl->linkingLibraries().size());
 }
 
 const char* ModuleManifest::linkerLibrary(unsigned int index) const
 {
-    return impl->linkingLibraries[index].c_str();
+    return impl->linkingLibraries()[index].c_str();
 }
 
 ModuleType ModuleManifest::moduleType() const
 {
-    return impl->type;
+    return impl->module.type;
 }
 
 unsigned int ModuleManifest::dependencies() const
 {
-    return static_cast<unsigned int>(impl->moduleDependencies.size());
+    return static_cast<unsigned int>(impl->module.moduleDependencies.size());
 }
 
 ModuleManifest* ModuleManifest::dependency(unsigned int index) const
 {
-    return impl->moduleDependencies[index];
+    return impl->module.moduleDependencies[index];
 }
 
 unsigned int ModuleManifest::includePaths(AccessDomain type) const
 {
     switch (type) {
     case AD_Private:
-        return static_cast<unsigned int>(impl->privateIncludes.size());
+        return static_cast<unsigned int>(impl->privateIncludes().size());
     case AD_Public:
-        return static_cast<unsigned int>(impl->publicIncludes.size());
+        return static_cast<unsigned int>(impl->publicIncludes().size());
     }
     return 0;
 }
@@ -196,21 +220,21 @@ const char* ModuleManifest::includePath(AccessDomain type, unsigned int index) c
 {
     switch (type) {
     case AD_Private:
-        return impl->privateIncludes[index].c_str();
+        return impl->privateIncludes()[index].c_str();
     case AD_Public:
-        return impl->publicIncludes[index].c_str();
+        return impl->publicIncludes()[index].c_str();
     }
     return nullptr;
 }
 
 unsigned int ModuleManifest::sourceFiles() const
 {
-    return static_cast<unsigned int>(impl->sourceFiles.size());
+    return static_cast<unsigned int>(impl->module.sourceFiles.size());
 }
 
 const char* ModuleManifest::sourceFile(unsigned int index) const
 {
-    return impl->sourceFiles[index].c_str();
+    return impl->module.sourceFiles[index].c_str();
 }
 
 void ModuleManifest::scanSource(const char* filter) const
@@ -229,17 +253,17 @@ void ModuleManifest::scanSource(const char* filter) const
 
 unsigned int ModuleManifest::mounts() const
 {
-    return static_cast<unsigned int>(impl->folderMounts.size());
+    return static_cast<unsigned int>(impl->module.folderMounts.size());
 }
 
 const FolderMount& ModuleManifest::mount(unsigned int index) const
 {
-    return impl->folderMounts[index];
+    return impl->module.folderMounts[index];
 }
 
 const char* ModuleManifest::uuid() const
 {
-    return impl->uuid.c_str();
+    return impl->module.uuid.c_str();
 }
 
 bool ModuleManifest::configure()
@@ -304,49 +328,49 @@ bool ModuleManifest::loadCache(const BuildSetup& config)
     std::ifstream filesList(cacheFolder / "Source.txt");
     while (std::getline(filesList, line))
     {
-        impl->sourceFiles.push_back(line);
+        impl->module.sourceFiles.push_back(line);
     }
     filesList.close();
 
     std::ifstream publicIncludeList(cacheFolder / "PublicIncludes.txt");
     while (std::getline(publicIncludeList, line))
     {
-        impl->publicIncludes.push_back(line);
+        impl->module.publicIncludes.push_back(line);
     }
     publicIncludeList.close();
 
     std::ifstream privateIncludeList(cacheFolder / "PrivateIncludes.txt");
     while (std::getline(privateIncludeList, line))
     {
-        impl->privateIncludes.push_back(line);
+        impl->module.privateIncludes.push_back(line);
     }
     privateIncludeList.close();
 
     std::ifstream publicDefineList(cacheFolder / "PublicDefines.txt");
     while (std::getline(publicDefineList, line))
     {
-        impl->publicDefines.push_back(line);
+        impl->module.publicDefines.push_back(line);
     }
     publicDefineList.close();
 
     std::ifstream privateDefineList(cacheFolder / "PrivateDefines.txt");
     while (std::getline(privateDefineList, line))
     {
-        impl->privateDefines.push_back(line);
+        impl->module.privateDefines.push_back(line);
     }
     privateDefineList.close();
 
     std::ifstream dependencyList(cacheFolder / "Dependencies.txt");
     while (std::getline(dependencyList, line))
     {
-        impl->moduleDependencyPaths.push_back(line);
+        impl->module.moduleDependencyPaths.push_back(line);
     }
     dependencyList.close();
 
     std::ifstream libraryList(cacheFolder / "Libraries.txt");
     while (std::getline(libraryList, line))
     {
-        impl->linkingLibraries.push_back(line);
+        impl->module.linkingLibraries.push_back(line);
     }
     libraryList.close();
 
@@ -358,18 +382,18 @@ bool ModuleManifest::loadCache(const BuildSetup& config)
 
     if (moduleTypeString == "lib")
     {
-        impl->type = MT_Library;
+        impl->module.type = MT_Library;
     }
     else if (moduleTypeString == "app")
     {
-        impl->type = MT_Executable;
+        impl->module.type = MT_Executable;
     }
 
     char uuid[UUID_LENGTH];
     std::filesystem::path uuidStorage = cacheFolder / "ModuleId.txt";
     if (Utility::GetCachedUUID(uuidStorage.string().c_str(), uuid))
     {
-        impl->uuid = uuid;
+        impl->module.uuid = uuid;
     }
 
     std::ifstream mountList(cacheFolder / "Mounts.txt");
@@ -379,7 +403,7 @@ bool ModuleManifest::loadCache(const BuildSetup& config)
         if (!std::getline(mountList, location)) break;
         if (!std::getline(mountList, mountPoint)) break;
 
-        impl->folderMounts.push_back({location.c_str(), mountPoint.c_str()});
+        impl->module.folderMounts.push_back({location.c_str(), mountPoint.c_str()});
     }
     mountList.close();
 
@@ -391,56 +415,56 @@ bool ModuleManifest::saveCache(const BuildSetup& config) const
     std::filesystem::path cacheFolder = getBuildSetupPath(this, config);
 
     std::ofstream filesList(cacheFolder / "Source.txt");
-    for (std::string& path : impl->sourceFiles)
+    for (std::string& path : impl->module.sourceFiles)
     {
         filesList << path << "\n";
     }
     filesList.close();
 
     std::ofstream publicIncludeList(cacheFolder / "PublicIncludes.txt");
-    for (std::string& path : impl->publicIncludes)
+    for (std::string& path : impl->module.publicIncludes)
     {
         publicIncludeList << path << "\n";
     }
     publicIncludeList.close();
 
     std::ofstream privateIncludeList(cacheFolder / "PrivateIncludes.txt");
-    for (std::string& path : impl->privateIncludes)
+    for (std::string& path : impl->module.privateIncludes)
     {
         privateIncludeList << path << "\n";
     }
     privateIncludeList.close();
 
     std::ofstream publicDefineList(cacheFolder / "PublicDefines.txt");
-    for (std::string& path : impl->publicDefines)
+    for (std::string& path : impl->module.publicDefines)
     {
         publicDefineList << path << "\n";
     }
     publicDefineList.close();
 
     std::ofstream privateDefinesList(cacheFolder / "PrivateDefines.txt");
-    for (std::string& path : impl->privateDefines)
+    for (std::string& path : impl->module.privateDefines)
     {
         privateDefinesList << path << "\n";
     }
     privateDefinesList.close();
 
     std::ofstream dependencyList(cacheFolder / "Dependencies.txt");
-    for (ModuleManifest* dependency : impl->moduleDependencies)
+    for (ModuleManifest* dependency : impl->module.moduleDependencies)
     {
         dependencyList << dependency->manifestPath() << "\n";
     }
     dependencyList.close();
 
     std::ofstream libraryList(cacheFolder / "Libraries.txt");
-    for (std::string& path : impl->linkingLibraries)
+    for (std::string& path : impl->module.linkingLibraries)
     {
         libraryList << path.c_str() << "\n";
     }
     libraryList.close();
 
     std::ofstream moduleType(cacheFolder / "ModuleType.txt");
-    switch (impl->type)
+    switch (impl->module.type)
     {
         case MT_Library: { moduleType << "lib"; break; }
         case MT_Executable: { moduleType << "app"; break; }
@@ -448,7 +472,7 @@ bool ModuleManifest::saveCache(const BuildSetup& config) const
     moduleType.close();
 
     std::ofstream mountList(cacheFolder / "Mounts.txt");
-    for (FolderMount& mount : impl->folderMounts)
+    for (FolderMount& mount : impl->module.folderMounts)
     {
         mountList << mount.location() << "\n" << mount.mountPoint() << "\n";
     }
