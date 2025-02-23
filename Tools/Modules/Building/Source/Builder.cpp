f3265a6623aeac4b9caf836a7f067846fda48349
R00tDroid <info@rootdroid.nl>
2025-02-23 12:05:31 +0100

    Get toolchain

--- Tools/Modules/Building/Source/Builder.cpp
+++ Tools/Modules/Building/Source/Builder.cpp
@@ -1,6 +1,20 @@
 #include "Builder.hpp"
+#include "Utility.hpp"
 
-bool Builder::build(const BuildSetup&, const ProjectManifest*, unsigned int, const ModuleManifest**)
+bool Builder::build(const BuildSetup& buildSetup, const ProjectManifest*, unsigned int, const ModuleManifest**)
 {
+    if (buildSetup.target == nullptr)
+    {
+        Utility::PrintLine("Invalid build target");
+        return false;
+    }
+
+    const Toolchain* toolchain = buildSetup.target->toolchain();
+    if (toolchain == nullptr)
+    {
+        Utility::PrintLine("Invalid toolchain for build target");
+        return false;
+    }
+
     return false;
 }
