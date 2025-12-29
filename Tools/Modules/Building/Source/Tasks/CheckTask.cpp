7aaab1abb1c64f655ee035f4266cc66c87d762b0
R00tDroid <info@rootdroid.nl>
2025-12-29 20:40:14 +0100

    Store in tree

--- Tools/Modules/Building/Source/Tasks/CheckTask.cpp
+++ Tools/Modules/Building/Source/Tasks/CheckTask.cpp
@@ -2,10 +2,34 @@
 #include "Toolchain.hpp"
 #include "Utility.hpp"
 
+void FileTree::addFile(std::filesystem::path file, std::vector<std::filesystem::path> branches)
+{
+    filesLock.lock();
+
+    auto it = files.find(file);
+    if (it == files.end()) { files.insert(std::pair(file, new FileTreeEntry())); }
+
+    FileTreeEntry* entry = files[file];
+    entry->file = file;
+
+    for (const std::filesystem::path& branch : branches)
+    {
+        entry->branches.insert(branch);
+
+        auto branchIt = files.find(file);
+        if (branchIt == files.end()) { files.insert(std::pair(branch, new FileTreeEntry())); }
+
+        FileTreeEntry* branchEntry = files[branch];
+        branchEntry->trunks.insert(file);
+    }
+
+    filesLock.unlock();
+}
+
 void ModuleCheckStep::start()
 {
     moduleBuild()->pool.addTask<ModuleBinCheckTask>(moduleBuild());
-    moduleBuild()->pool.addTask<ModuleDatabaseCheckTask>(moduleBuild());
+    moduleBuild()->pool.addTask<ModuleDatabaseCheckTask>(this);
 }
 
 bool ModuleCheckStep::end()
@@ -40,12 +64,12 @@ void ModuleBinCheckTask::runTask()
     }
 }
 
-ModuleDatabaseCheckTask::ModuleDatabaseCheckTask(ModuleBuild* info) : info(info) {}
+ModuleDatabaseCheckTask::ModuleDatabaseCheckTask(ModuleCheckStep* step) : step(step) {}
 
 void ModuleDatabaseCheckTask::runTask()
 {
     //TODO Check for saved database
-    Utility::PrintLineD("Check module changes " + std::string(info->module->name()));
+    Utility::PrintLineD("Check module changes " + std::string(step->moduleBuild()->module->name()));
 
     //TODO If no database, or changes detected, scan tree
     scheduleScans();
@@ -53,21 +77,26 @@ void ModuleDatabaseCheckTask::runTask()
 
 void ModuleDatabaseCheckTask::scheduleScans()
 {
-    for (unsigned int sourceIndex = 0; sourceIndex < info->module->sourceFiles(); sourceIndex++)
+    for (unsigned int sourceIndex = 0; sourceIndex < step->moduleBuild()->module->sourceFiles(); sourceIndex++)
     {
-        std::filesystem::path file = info->module->sourceFile(sourceIndex);
+        std::filesystem::path file = step->moduleBuild()->module->sourceFile(sourceIndex);
 
         if (Utility::IsSourceFile(file.string().c_str()))
         {
             Utility::PrintLineD("Schedule scan for " + std::string(file.string().c_str()));
-            info->pool.addTask<ModuleScanTask>(info, file);
+            step->moduleBuild()->pool.addTask<ModuleScanTask>(step, file);
         }
     }
+
+    //TODO Await tree and check dates
 }
 
-ModuleScanTask::ModuleScanTask(ModuleBuild* info, std::filesystem::path file) : info(info), file(file) {}
+ModuleScanTask::ModuleScanTask(ModuleCheckStep* step, std::filesystem::path file) : step(step), file(file) {}
 
 void ModuleScanTask::runTask()
 {
     Utility::PrintLineD("Scanning " + std::string(file.string().c_str()));
+
+    //TODO Scan for includes and resolve to files paths
+    step->fileTree.addFile(file, {});
 }
--- Tools/Modules/Building/Source/Tasks/CheckTask.hpp
+++ Tools/Modules/Building/Source/Tasks/CheckTask.hpp
@@ -1,5 +1,24 @@
 #pragma once
+#include <map>
 #include "ModuleTaskInfo.hpp"
+
+struct FileTreeEntry
+{
+    std::filesystem::path file;
+
+    std::set<std::filesystem::path> branches;
+    std::set<std::filesystem::path> trunks;
+};
+
+class FileTree
+{
+public:
+    void addFile(std::filesystem::path file, std::vector<std::filesystem::path> branches);
+
+private:
+    std::mutex filesLock;
+    std::map<std::filesystem::path, FileTreeEntry*> files;
+};
 
 class ModuleCheckStep : public BuildStepInterface
 {
@@ -7,6 +26,11 @@ public:
     ModuleCheckStep(ModuleBuild* parent) : BuildStepInterface(parent) {}
     void start() override;
     bool end() override;
+
+    std::mutex scannedFilesLock;
+    std::vector<std::filesystem::path> scannedFiles;
+
+    FileTree fileTree;
 };
 
 class ModuleBinCheckTask : public WorkerTask
@@ -23,24 +47,24 @@ private:
 class ModuleDatabaseCheckTask : public WorkerTask
 {
 public:
-    ModuleDatabaseCheckTask(ModuleBuild* info);
+    ModuleDatabaseCheckTask(ModuleCheckStep* step);
 
     void runTask() override;
 
     void scheduleScans();
 
 private:
-    ModuleBuild* info;
+    ModuleCheckStep* step;
 };
 
 class ModuleScanTask : public WorkerTask
 {
 public:
-    ModuleScanTask(ModuleBuild* info, std::filesystem::path file);
+    ModuleScanTask(ModuleCheckStep* step, std::filesystem::path file);
 
     void runTask() override;
 
 private:
-    ModuleBuild* info;
+    ModuleCheckStep* step;
     std::filesystem::path file;
 };
