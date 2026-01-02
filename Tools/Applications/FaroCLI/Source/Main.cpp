c35963e395f29299fad219e444c88a5333a65558
R00tDroid <info@rootdroid.nl>
2026-01-02 16:47:15 +0100

    Remove newline

--- Tools/Applications/FaroCLI/Source/Main.cpp
+++ Tools/Applications/FaroCLI/Source/Main.cpp
@@ -31,7 +31,6 @@ int main(int argc, char** argv)
         taskInfo.runClean ||
         taskInfo.runDeploy)
     {
-
         if (taskInfo.runConfigure)
         {
             if (!project.configure())
