2ede6f02032afdb7f6eea2b43f67adf24721395f
R00tDroid <info@rootdroid.nl>
2026-01-03 09:17:09 +0100

    Test compile-time reflection

--- Tools/Modules/Reflection/Source/Reflector.cpp
+++ Tools/Modules/Reflection/Source/Reflector.cpp
@@ -1,7 +1,30 @@
 #include "Reflector.hpp"
+#include <filesystem>
+#include <fstream>
+#include "ModuleInfo.hpp"
 
-bool Reflector::generateFileReflection(const ModuleManifest*, unsigned int)
+bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, unsigned int fileIndex)
 {
+    std::filesystem::path file = moduleManifest->sourceFile(fileIndex);
+
+    std::ifstream fileStream(file);
+
+    fileStream.seekg(0, std::ios::end);
+    size_t size = fileStream.tellg();
+    std::string fileBuffer(size, ' ');
+    fileStream.seekg(0);
+    fileStream.read(fileBuffer.data(), size);
+
+    while (true)
+    {
+        size_t classStart = fileBuffer.find(" Class(");
+        if (classStart == std::string::npos) break;
+
+        fileBuffer.erase(fileBuffer.begin(), fileBuffer.begin() + classStart);
+
+        Utility::PrintLine("");
+    }
+
     return true;
 }
 
