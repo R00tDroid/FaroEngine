a17615a107599330306aafd58266e126e0ac65d6
R00tDroid <info@rootdroid.nl>
2026-01-14 21:39:31 +0100

    Try to match members

--- Tools/Modules/Reflection/Source/Reflector.cpp
+++ Tools/Modules/Reflection/Source/Reflector.cpp
@@ -22,6 +22,7 @@ bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, uns
     TSParser* tsParser = ts_parser_new();
     if (!ts_parser_set_language(tsParser, tree_sitter_cpp()))
     {
+        Utility::PrintLine("Failed to setup parser");
         return false;
     }
 
@@ -34,15 +35,32 @@ bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, uns
 
     TSNode root_node = ts_tree_root_node(tree);
 
+    char* tree_string = ts_node_string(root_node);
+    //Utility::PrintLine(file.filename().string() + ": " + tree_string);
+    free(tree_string);
+
     const char* query_string = R"(
-        (class_specifier 
-            name: (type_identifier) @class_name
-            body: (field_declaration_list
-                (field_declaration 
-                    declarator: (field_identifier) @member_name)
-            )
-        )
-    )";
+    (
+        (expression_statement 
+            (call_expression 
+                (identifier) @macro_name 
+                (argument_list) @flags)
+        ) 
+        
+        . 
+
+        [
+            (class_specifier name: (_) @class_name)
+            (field_declaration _ (_) @field_name)
+            (field_declaration _ (function_declarator (_) @field_name))
+            (declaration _ (_) @field_name)
+            (declaration _ (function_declarator (_) @field_name))
+        ]
+
+        ; Predicate to filter for "Reflect"
+        (#eq? @macro_name "Reflect")
+    )
+)";
 
     uint32_t error_offset;
     TSQueryError error_type;
@@ -55,14 +73,23 @@ bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, uns
         &error_type
     );
 
+    if (query == nullptr)
+    {
+        std::string error_context = std::string(query_string).substr(error_offset, 20);
+
+        Utility::PrintLine("Failed to setup parsing query: " + std::to_string(error_type) + ", " + error_context);
+        return false;
+    }
+
     TSQueryCursor* cursor = ts_query_cursor_new();
     ts_query_cursor_exec(cursor, query, root_node);
 
     TSQueryMatch match;
     while (ts_query_cursor_next_match(cursor, &match)) {
+        std::string current_flags = "";
+
         for (uint16_t i = 0; i < match.capture_count; i++) {
             TSQueryCapture capture = match.captures[i];
-
             uint32_t name_len;
             std::string capture_name = ts_query_capture_name_for_id(query, capture.index, &name_len);
 
@@ -70,7 +97,15 @@ bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, uns
             uint32_t end = ts_node_end_byte(capture.node);
             std::string text = fileBuffer.substr(start, end - start);
 
-            Utility::PrintLineD(capture_name + ": " + text);
+            if (capture_name == "flags") {
+                current_flags = text.substr(1, text.length() - 2);
+            }
+            else if (capture_name == "class_name") {
+                Utility::PrintLineD("Found Reflected Class: " + text + " with flags: [" + current_flags + "]");
+            }
+            else if (capture_name == "field_name") {
+                Utility::PrintLineD("Found Reflected Field: " + text + " with flags: [" + current_flags + "]");
+            }
         }
     }
 
