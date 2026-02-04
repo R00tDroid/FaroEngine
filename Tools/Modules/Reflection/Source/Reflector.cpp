c3e0c493465977a6acebeaea6d279a60f748df19
R00tDroid <info@rootdroid.nl>
2026-02-04 22:47:02 +0100

    Print tree and find identifiers

--- Tools/Modules/Reflection/Source/Reflector.cpp
+++ Tools/Modules/Reflection/Source/Reflector.cpp
@@ -8,6 +8,28 @@
 
 extern "C" const TSLanguage* tree_sitter_cpp(void);
 
+void print_node(TSNode& node, unsigned int indentation, std::string& fileBuffer)
+{
+    TSPoint start = ts_node_start_point(node);
+    
+    std::string nodeType = ts_node_type(node);
+    std::string text = {};
+    
+    if (nodeType.find("identifier") != std::string::npos) {
+        uint32_t textStart = ts_node_start_byte(node);
+        uint32_t textEnd = ts_node_end_byte(node);
+        text = fileBuffer.substr(textStart, textEnd - textStart);
+    } 
+
+    Utility::PrintLineD(std::string(indentation * 2, ' ') + " -" + nodeType + " (" + std::to_string(start.row) + ", " + std::to_string(start.column) + ") " + text);
+
+    uint32_t child_count = ts_node_named_child_count(node);
+    for (uint32_t i = 0; i < child_count; i++) {
+        TSNode child = ts_node_named_child(node, i);
+        print_node(child, indentation + 1, fileBuffer);
+    }
+}
+
 bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, unsigned int fileIndex)
 {
     std::filesystem::path file = moduleManifest->sourceFile(fileIndex);
@@ -18,6 +40,9 @@ bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, uns
     std::string fileBuffer(size, ' ');
     fileStream.seekg(0);
     fileStream.read(fileBuffer.data(), size);
+
+    //TODO What to do when nothing is reflect-able?
+    if (fileBuffer.find("Reflect") == std::string::npos) return true;
 
     TSParser* tsParser = ts_parser_new();
     if (!ts_parser_set_language(tsParser, tree_sitter_cpp()))
@@ -33,84 +58,9 @@ bool Reflector::generateFileReflection(const ModuleManifest* moduleManifest, uns
         static_cast<uint32_t>(fileBuffer.length())
     );
 
-    TSNode root_node = ts_tree_root_node(tree);
+    TSNode root = ts_tree_root_node(tree);
+    print_node(root, 0, fileBuffer);
 
-    char* tree_string = ts_node_string(root_node);
-    //Utility::PrintLine(file.filename().string() + ": " + tree_string);
-    free(tree_string);
-
-    const char* query_string = R"(
-    (
-        (expression_statement 
-            (call_expression 
-                (identifier) @macro_name 
-                (argument_list) @flags)
-        ) 
-        
-        . 
-
-        [
-            (class_specifier name: (_) @class_name)
-            (field_declaration _ (_) @field_name)
-            (field_declaration _ (function_declarator (_) @field_name))
-            (declaration _ (_) @field_name)
-            (declaration _ (function_declarator (_) @field_name))
-        ]
-
-        ; Predicate to filter for "Reflect"
-        (#eq? @macro_name "Reflect")
-    )
-)";
-
-    uint32_t error_offset;
-    TSQueryError error_type;
-
-    TSQuery* query = ts_query_new(
-        tree_sitter_cpp(),
-        query_string,
-        static_cast<uint32_t>(strlen(query_string)),
-        &error_offset,
-        &error_type
-    );
-
-    if (query == nullptr)
-    {
-        std::string error_context = std::string(query_string).substr(error_offset, 20);
-
-        Utility::PrintLine("Failed to setup parsing query: " + std::to_string(error_type) + ", " + error_context);
-        return false;
-    }
-
-    TSQueryCursor* cursor = ts_query_cursor_new();
-    ts_query_cursor_exec(cursor, query, root_node);
-
-    TSQueryMatch match;
-    while (ts_query_cursor_next_match(cursor, &match)) {
-        std::string current_flags = "";
-
-        for (uint16_t i = 0; i < match.capture_count; i++) {
-            TSQueryCapture capture = match.captures[i];
-            uint32_t name_len;
-            std::string capture_name = ts_query_capture_name_for_id(query, capture.index, &name_len);
-
-            uint32_t start = ts_node_start_byte(capture.node);
-            uint32_t end = ts_node_end_byte(capture.node);
-            std::string text = fileBuffer.substr(start, end - start);
-
-            if (capture_name == "flags") {
-                current_flags = text.substr(1, text.length() - 2);
-            }
-            else if (capture_name == "class_name") {
-                Utility::PrintLineD("Found Reflected Class: " + text + " with flags: [" + current_flags + "]");
-            }
-            else if (capture_name == "field_name") {
-                Utility::PrintLineD("Found Reflected Field: " + text + " with flags: [" + current_flags + "]");
-            }
-        }
-    }
-
-    ts_query_cursor_delete(cursor);
-    ts_query_delete(query);
     ts_tree_delete(tree);
     ts_parser_delete(tsParser);
 
