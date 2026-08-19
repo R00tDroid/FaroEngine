b73521a0f4f3092cc4f1fc744a265813f7f4d7f9
R00tdroid <info@rootdroid.nl>
2026-08-19 14:16:44 +0200

    Keep project reference in module manifest

--- Tools/Modules/Project/Source/ModuleInfo.hpp
+++ Tools/Modules/Project/Source/ModuleInfo.hpp
@@ -42,7 +42,7 @@ class FaroProjectsExports ModuleManifest : public IManifest
 public:
     static const char* moduleManifestExtension();
 
-    ModuleManifest(const char* manifestLocation);
+    ModuleManifest(const char* manifestLocation, ProjectManifest* project);
     ~ModuleManifest() override;
 
     // Name of the module
