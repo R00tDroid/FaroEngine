3b92994a96069064c80cf527b4bb2dbe8ecd4de4
R00tdroid <info@rootdroid.nl>
2026-08-20 12:55:15 +0200

    Track type

--- Source/Engine/Core/Source/Util/ReflectionTypes.hpp
+++ Source/Engine/Core/Source/Util/ReflectionTypes.hpp
@@ -1,38 +1,51 @@
 #pragma once
 #include <vector>
 
+enum ReflectionTypeId
+{
+    Class,
+    ClassVariable,
+    ClassFunction,
+};
+
 class ReflectedType {
 protected:
-    ReflectedType(const char* name): name(name) {}
+    ReflectedType(const char* name, ReflectionTypeId typeId): name(name), typeId(typeId) {}
 
 private:
     const char* name;
+    ReflectionTypeId typeId;
 
 public:
     const char* GetName() const
     {
         return name;
     }
+
+    ReflectionTypeId GetTypeId() const
+    {
+        return typeId;
+    }
 };
 
 class ReflectedClassMember : public ReflectedType {
 public:
-    ReflectedClassMember(const char* name): ReflectedType(name) {}
+    ReflectedClassMember(const char* name, ReflectionTypeId typeId): ReflectedType(name, typeId) {}
 };
 
 class ReflectedClassMemberVariable : public ReflectedClassMember {
 public:
-    ReflectedClassMemberVariable(const char* name): ReflectedClassMember(name) {}
+    ReflectedClassMemberVariable(const char* name): ReflectedClassMember(name, ClassVariable) {}
 };
 
 class ReflectedClassMemberFunction : public ReflectedClassMember {
 public:
-    ReflectedClassMemberFunction(const char* name): ReflectedClassMember(name) {}
+    ReflectedClassMemberFunction(const char* name): ReflectedClassMember(name, ClassFunction) {}
 };
 
 class ReflectedClass : public ReflectedType {
 public:
-    ReflectedClass(const char* name, std::vector<const char*> baseTypes, std::vector<ReflectedClassMember*> members) : ReflectedType(name), baseTypes(baseTypes) {}
+    ReflectedClass(const char* name, std::vector<const char*> baseTypes, std::vector<ReflectedClassMember*> members) : ReflectedType(name, Class), baseTypes(baseTypes) {}
 
     std::vector<const char*> baseTypes;
 };
